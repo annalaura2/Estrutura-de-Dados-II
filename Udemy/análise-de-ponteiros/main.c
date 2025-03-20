845aadbe9460c2f4b6ae80d7d8090f599562562c
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2025-03-20 13:25:57 -0300

    Update main.c

--- Udemy/análise-de-ponteiros/main.c
+++ Udemy/análise-de-ponteiros/main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void){
+
+    int a1 = 10;
+    int *a2 = &a1; 
+
+    int b1 = 27;
+    int *b2 = &b1; 
+
+    printf("valor: %d, endereço: %p\n", a1, a2);
+    printf("valor = %d, endereço: %p\n\n", b1, b2);
+
+    //alteração de valores
+
+    *a2 = 20; 
+    *b2 = 100; 
+
+    printf("valor = %d,endereço = %p\n", a1, a2); 
+    printf("valor = %d,endereço = %p\n\n", b1, b2);
+
+}
